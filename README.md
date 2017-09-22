adb54b1a48e0688f0a54668a23e6bf7def43d3c8
Yuri Kalinin <yuri@yuris-mbp.home>
2017-09-22 15:42:07 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# express-joi-params
-Validate express input all kind of parameters with joi
+## Validation middleware for express with joi library
+This is easy to use simple validator middleware for express routes.
+No need to validate input parameters manually, just configure route middleware.
+Uses Joi library:
+https://github.com/hapijs/joi
+
+### Install with npm
+```
+  npm install express-joi-params
+```
+
+### Use
+
+In your express route file include
+
+```
+const validation = require('express-joi-params');
+const rule = require('./validation').rule;
+```
+then in any rote add middleware
+
+```
+router.post('/api/login', validation.validateRequest({
+  body: {
+    login: rule.string().required(),
+    password: rule.string().required(),
+  },
+}), authController.login);
+```
+
+in case of error will output
+```
+{
+    "message": "Some parameters are missing or invalid",
+    "success": false,
+    "errors": {
+        "errors": [
+            {
+                "message": "\"username\" is required",
+                "type": "any.required",
+                "field": [
+                    "username"
+                ]
+            }
+        ],
+        "type": "body"
+    }
+}
+```
+also will give 400 http code and will stop execution
+
+
+## [MIT License](https://github.com/georgearrowwood/resize-to-s3/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,75 @@
+const joi = require('joi');
+
+const validation = {
+  rule: joi,
+
+  validateRequest(schema) {
+    return (req, res, next) => {
+      this.validateInput(req, schema)
+        // moving forward when it's ok
+        .then(next)
+        .catch(err => this.handleError(err, res));
+    };
+  },
+
+  validateInput(req, schema) {
+    const validations = [
+      this.validateSchema(req.params, schema.params, 'params'),
+      this.validateSchema(req.query, schema.query, 'query'),
+      this.validateSchema(req.body, schema.body, 'body'),
+      this.validateSchema(req.files, schema.files, 'files'),
+    ];
+    return Promise.all(validations);
+  },
+
+  // Show error
+  handleError(err, res) {
+    res.status(400)
+      .json({
+        message: 'Some parameters are missing or invalid',
+        success: false,
+        errors: {
+          errors: err.errors,
+          type: err.type,
+        },
+      });
+  },
+
+  // Validate segment parameters
+  validateSchema(data, schema, type) {
+    if (typeof data === 'undefined') data = {};
+    if (!schema) return Promise.resolve();
+    return this.validate(data, schema, type);
+  },
+
+  // Validate set of data
+  validate(data, schema, type) {
+    return new Promise((resolve, reject) => {
+      if (schema) {
+        let wrappedSchema = Object.assign({}, schema);
+        wrappedSchema = joi.object(wrappedSchema).required();
+        joi.validate(data, wrappedSchema, (err, value) => {
+          if (err) {
+            // there are validation errors
+            return reject({
+              errors: err.details.map(this.mapJoiErrors),
+              type: type,
+            });
+          }
+          // validation has passed
+          return resolve();
+        });
+      } else {
+        return reject({ code: 'no-validation-scheme', message: 'No Scheme found for validation.' });
+      }
+    });
+  },
+
+  mapJoiErrors: item => ({
+    message: item.message,
+    type: item.type,
+    field: item.path,
+  }),
+};
+
+module.exports = validation;
